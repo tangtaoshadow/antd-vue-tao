dd354c54572b51ebed871d38e207aca297bcd86e
tangtao <tangtaoshadow@gmail.com>
2019-11-17 01:43:34 +0800

    author tangtao change initialization time 2019-11-17 01:43:24

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: TangTao https://www.promiselee.cn/tao
+ * @Date: 2019-11-17 01:43:07
+ * @Last Modified by:   TangTao tangtao2099@outlook.com
+ * @Last Modified time: 2019-11-17 01:43:07
+ */
 import Vue from "vue";
 import App from "./App.vue";
 // import Button from "ant-design-vue/lib/button";
